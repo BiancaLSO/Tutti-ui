b23919842fed7805fccd0c0a134ea539ef8da430
Maria Otelea <otelea.lavi@gmail.comm>
2022-11-15 15:45:12 +0100

    created and styled signup form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import styles from  "./Signup.module.css"
 
 export default function SignUp() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPassword] = useState("");
   const [isMusician, setIsMusician] = useState(false);
+
 
   async function handleSubmit() {
     let user = { username, email, password, confirmPassword, isMusician };
@@ -22,59 +24,64 @@ export default function SignUp() {
 
   return (
     <>
-      <form method="POST" action="http://localhost:3000/signup">
-        <label>
+    <nav>
+This is a navigation
+    </nav>
+    <h2>Sign Up</h2>
+      <form method="POST" action="http://localhost:3000/signup" className={styles}>
+        <label>Username
           <input
             type="text"
-            placeholder="name"
-            name="username"
+            placeholder="Username"
+            name="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </label>
 
-        <label>
+        <label>E-mail
           <input
             type="email"
-            placeholder="email"
+            placeholder="E-mail"
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
 
-        <label>
+        <label>Password
           <input
             type="password"
-            placeholder="password"
+            placeholder="Password"
             name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
-
-        <label>
+<label> Are you a musician?   </label>
+           
+        <div className={styles.radio}>
           <input
-            type="password"
-            placeholder=" Confirm password"
-            name="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </label>
-
-        <label>
-          <input
-            type="text"
-            placeholder="Are you a musician?"
+            type="radio"
             name="isMusician"
             value={isMusician}
             onChange={(e) => setIsMusician(e.target.value)}
-          />
-        </label>
+          />Yes
+        </div>
+        <div className={styles.radio}>
+        <input
+            type="radio"
+            name="isMusician"
+            value={isMusician}
+            onChange={(e) => setIsMusician(e.target.value)}
+          />No
+        </div>
 
         <input type="submit" onClick={handleSubmit} value="Sign Up" />
       </form>
+      <footer>
+This is a footer
+      </footer>
     </>
   );
 }
