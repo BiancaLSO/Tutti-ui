a89cbc008113b9ef8f8bb402320f6468c4ec8279
Bianca <biancastan90@gmail.com>
2022-11-16 13:19:36 +0100

    Added routing for empty pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import SignUp from "./components/Signup";
 import LogIn from "./components/Login";
 import { Route, Routes } from "react-router-dom";
+import MusicianProfile from "./components/MusicianProfile";
+import EnsembleProfile from "./components/EnsembleProfile";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<SignUp />} />
       <Route path="/auth/login" element={<LogIn />} />
+      <Route path="/musician" element={<MusicianProfile />} />
+      <Route path="/ensemble" element={<EnsembleProfile />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
---
+++ src/components/MusicianProfile.jsx
@@ -0,0 +1,3 @@
+export default function MusicianProfile() {
+  return <div>MusicianProfile page</div>;
+}
