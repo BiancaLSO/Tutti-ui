518572b509b0df605a6c64277f4ec735c5fd0f30
Maria Otelea <otelea.lavi@gmail.comm>
2022-12-06 15:13:15 +0100

    Redesigned the loggedin user display

--- src/components/MusicianProfile.jsx
+++ src/components/MusicianProfile.jsx
@@ -1,15 +1,31 @@
 import styles from "./Profile.module.css";
+import style from "./Signup.module.css";
 import Navigation from "./shared/Navigation";
 import Footer from "./shared/Footer";
 import { useState, useEffect } from "react";
+import ShowModal from "./Modal";
 
 
 export default function MusicianProfile() {
 
   
   const [user, setUser] = useState([]);
+  const [isShown, setIsShown] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
+  const handleModal = () => {
+    setShowModal(true);
+  };
   
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleClick = () => {
+    setIsShown(current => !current);
+
+  };
+
   const getToken = () => {
     return localStorage.getItem("token");
   };
@@ -49,72 +65,87 @@ export default function MusicianProfile() {
   return (
     <>
       <Navigation></Navigation>
+      
       {/* <h2 st>Create a profile</h2> */}
-    <h1 className={styles.heading}>Hi, { user.username}!</h1>
+    <h1 className={styles.heading}>Hi, { user.username}! &#128075;</h1>
+    {!isShown && (
     <div className={styles.grid}>
+
     <div className={styles.anotherColumn}>
     <img src = "https://i.pravatar.cc/300"  className={styles.profile}></img>
-    <button className={styles.update}>Update profile</button>
+    <button className={styles.contact} onClick={handleModal}>Contact</button>
     </div>
+
     <div className={styles.column}>
-      <h3 className={styles.secondHeading}>Basic Information</h3>
-      <div className={styles.info}>Full name: <span className={styles.span}>{ user.username}</span></div>
-      <div className={styles.info}>E-mail: <span className={styles.span}>{ user.email}</span></div>
-      <div className={styles.info}>Phone number: <span className={styles.span}>{ user.phoneNo}</span></div>
-      <div className={styles.info}>Played Instrument: <span className={styles.span}>{ user.instrument}</span></div>
-      <div className={styles.info}>About me: <span className={styles.span}>{ user.description}</span></div>
-      <button className={styles.delete}>Delete profile</button>
+      <div className={styles.info}>{ user.fullName}</div>
+      <div className={styles.instrum}><span className={styles.span}> &#127925;  {user.instrument}</span></div>
+      <div className={styles.infodesc}>About me: </div> 
+      <span className={styles.span}>{ user.description}</span>
+
+      <div className={styles.buttons}>
+      <button className={styles.update} onClick={handleClick}>Update profile</button>
+        <button className={styles.delete}>Delete profile</button>
+        </div>
+       
     </div>
+
     </div>
-    
+    )}
+     {!isShown && (
     <div className={styles.gridEnsembles}>
     <h1 className={styles.heading}>My Ensembles</h1>
     </div>
+  )}
 
-      {/* <div className={styles.center}>
-        <form className={styles}>
-          <label>
-            Phone number
-            <input
-              type="text`"
-              placeholder="Username"
-              name="username"
-              value={user.phoneNo}
-            />
-          </label> */}
+  {isShown && (
+    
+      <div className={style.center}>
+     
+        <form className={style}>
 
-          {/* <label>
-            E-mail
-            <input
-              type="email"
-              placeholder="E-mail"
-              name="email"
-              value={email}
-
-            />
-          </label>
-
-          <label>
-            Password
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={password}
-
-            />
-          </label>
-
-          <label>
+        <label>
             Full Name
             <input
               type="text"
               placeholder=" Full name"
               name="name"
-              value={fullName}
+              value={user.fullName}
 
             />
           </label>
+
+          <label>
+           Username
+            <input
+              type="text`"
+              placeholder="Username"
+              name="username"
+              value={user.username}
+            />
+          </label>
+
+          <label>
+            E-mail
+            <input
+              type="email"
+              placeholder="E-mail"
+              name="email"
+              value={user.email}
+
+            />
+          </label>
+
+          {/* <label>
+            Password
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={user.password}
+
+            />
+          </label> */}
+
 
           <label>
             Phone Number
@@ -122,7 +153,7 @@ export default function MusicianProfile() {
               type="number"
               placeholder="Phone Number"
               name="number"
-              value={phoneNo}
+              value={user.phoneNo}
             />
           </label>
 
@@ -132,7 +163,7 @@ export default function MusicianProfile() {
               type="text"
               placeholder="Instrument"
               name="instrument"
-              value={instrument}
+              value={user.instrument}
 
             />
           </label>
@@ -142,14 +173,16 @@ export default function MusicianProfile() {
             <textarea
               placeholder="Description"
               name="description"
-              value={description}
+              value={user.description}
 
             />
-          </label> */}
+          </label>
 
-          {/* <input type="submit" value="Update Profile" />
-        </form> */}
-      {/* </div> */}
+          <input type="submit" value="Update Profile" />
+        </form>
+      </div>
+         )}
+      <ShowModal showModal={showModal} closeModal={closeModal} />
       <Footer></Footer>
     </>
   );
