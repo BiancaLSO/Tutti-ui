34e72a4147cbff45ab428753634413864b77ab1f
Maria Otelea <otelea.lavi@gmail.comm>
2022-12-12 10:02:06 +0100

    Made prettier work

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -1,14 +1,12 @@
-import styles from "./Navigation.module.css"
+import styles from "./Navigation.module.css";
 import { useNavigate } from "react-router-dom";
 
 export default function Navigation() {
-
   const getToken = () => {
     return localStorage.getItem("token");
   };
 
   const tokenFromStorage = getToken();
-
 
   const navigate = useNavigate();
 
@@ -18,30 +16,44 @@ export default function Navigation() {
   };
 
   return (
-   <nav className={styles.navbar}>
-    <div className={styles.gridLeft}>
-    <h1>Musik Samspil</h1>
-    <p>Skabt af DAOS - Dansk Amatørorkester Samvirke </p>
-    </div>
-    <ul className={styles.navLinks}>
-  
-       <input type="checkbox" id="checkbox_toggle" />
-       <label htmlFor="checkbox_toggle" className={styles.hamburger}>&#9776;</label>
+    <nav className={styles.navbar}>
+      <div className={styles.gridLeft}>
+        <h1>Musik Samspil</h1>
+        <p>Skabt af DAOS - Dansk Amatørorkester Samvirke </p>
+      </div>
+      <ul className={styles.navLinks}>
+        <input type="checkbox" id="checkbox_toggle" />
+        <label htmlFor="checkbox_toggle" className={styles.hamburger}>
+          &#9776;
+        </label>
 
-       <div className={styles.menu}>
-         <li><a href="/">Posts</a></li>
-         <li style={{display: tokenFromStorage ? 'block' : 'none'}} className={styles.profile} ><a href="/musician">Profile</a></li>
-         {/* <li className={styles.services}>
+        <div className={styles.menu}>
+          <li>
+            <a href="/">Posts</a>
+          </li>
+          <li
+            style={{ display: tokenFromStorage ? "block" : "none" }}
+            className={styles.profile}
+          >
+            <a href="/musician">Profile</a>
+          </li>
+          {/* <li className={styles.services}>
            <a href="/ensemble">Ensemble Profile</a>
          </li> */}
- <li style={{display: tokenFromStorage ? 'none' : 'block'}}><a href="/signup">Create Account</a></li>
-<li style={{display: tokenFromStorage ? 'none' : 'block'}}><a href="/auth/login">Log In</a></li>
-<li style={{display: tokenFromStorage ? 'block' : 'none'}} onClick={logout}><a href="/auth/login">Log Out</a></li>
-       </div>
-     </ul>
-
+          <li style={{ display: tokenFromStorage ? "none" : "block" }}>
+            <a href="/signup">Create Account</a>
+          </li>
+          <li style={{ display: tokenFromStorage ? "none" : "block" }}>
+            <a href="/auth/login">Log In</a>
+          </li>
+          <li
+            style={{ display: tokenFromStorage ? "block" : "none" }}
+            onClick={logout}
+          >
+            <a href="/auth/login">Log Out</a>
+          </li>
+        </div>
+      </ul>
     </nav>
-
   );
-
 }
