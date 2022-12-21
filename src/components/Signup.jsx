4ad6ad0ee2dbccc4888a3ae3414633d072d8167d
Lauraatii <tihanova.laura@gmail.com>
2022-12-21 13:21:40 +0100

    validation changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState} from "react";
+import { useForm } from "react"
 import styles from "./Signup.module.css";
 import Navigation from "./shared/Navigation";
 import Footer from "./shared/Footer";
 import { useNavigate } from "react-router-dom";
-import { validateUsername } from "./../utils.js";
-import { validateEmail } from "./../utils.js";
-import { validatePassword } from "./../utils.js";
 
 export default function SignUp() {
   localStorage.clear();
   const navigate = useNavigate();
+  const { register, formState: { errors } } = useForm();
+  
 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
@@ -19,17 +19,10 @@ export default function SignUp() {
   const [instrument, setInstrument] = useState("");
   const [description, setDescription] = useState("");
   const [ensmebles] = useState([]);
-  const [usernameError, setUsernameError] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [isUsernameValid, setUsernameIsValid] = useState(true);
-  const [isEmailValid, setEmailIsValid] = useState(true);
-  const [isPasswordValid, setPasswordIsValid] = useState(true);
 
-
-  const onUsernameChange = (e) => {setUsername(e.target.value); setUsernameIsValid(true);};
-  const onEmailChange = (e) => {setEmail(e.target.value); setEmailIsValid(true); };
-  const onPasswordChange = (e) => {setPassword(e.target.value); setPasswordIsValid(true);}
+  const onUsernameChange = (e) => setUsername(e.target.value);
+  const onEmailChange = (e) => setEmail(e.target.value);
+  const onPasswordChange = (e) => setPassword(e.target.value);
   const onNameChange = (e) => setFullName(e.target.value);
   const onPhoneNoChange = (e) => setPhoneNo(e.target.value);
   const onInstrumentChange = (e) => setInstrument(e.target.value);
@@ -47,18 +40,6 @@ export default function SignUp() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    // const valid = validateUsername(setUsernameError, username);
-
-    const valid = validateUsername(username);
-    const validE = validateEmail(email);
-    const validp = validatePassword(password);
-
-    if (valid && validE && validp) {
-      setUsernameError("");
-      setEmailError("");
-      setPasswordError("");
-      console.log("send mail to server");
-
     const data = {
       username,
       email,
@@ -85,22 +66,13 @@ export default function SignUp() {
 
     navigate("/auth/login");
     clearForm();
-  } else {
-    setUsernameIsValid(false);
-    setEmailIsValid(false);
-    setPasswordIsValid(false);
-    setUsernameError ("Please enter username");
-    setEmailError ("Please enter email");
-    setPasswordError("Please enter password");}
-
-  }  
+  }
 
   return (
     <>
       <Navigation></Navigation>
       <div className={styles.center}>
-        <form
-        className={styles} onSubmit={handleSubmit}>
+        <form className={styles} onSubmit={handleSubmit ((data) => console.log(data))}>
           <label>
             Username
             <input
@@ -109,9 +81,9 @@ export default function SignUp() {
               name="username"
               value={username}
               onChange={onUsernameChange}
+              ref={register({ required: true })}
             />
-           { !isUsernameValid  ? <p style={{ color: "red" }}>{usernameError}</p> : 
-           <p style={{ display: "none" }}> </p> }
+            {errors.singleErrorInput && <p>Your input is required</p>}
           </label>
 
           <label>
@@ -122,9 +94,8 @@ export default function SignUp() {
               name="email"
               value={email}
               onChange={onEmailChange}
-              />
-            { !isEmailValid ? <p style={{ color: "red" }}>{emailError}</p> : 
-           <p style={{ display: "none" }}> </p> }
+              {...register}
+            />
           </label>
 
           <label>
@@ -135,9 +106,9 @@ export default function SignUp() {
               name="password"
               value={password}
               onChange={onPasswordChange}
-              />
-              { !isPasswordValid ? <p style={{ color: "red" }}>{passwordError}</p> : 
-           <p style={{ display: "none" }}> </p> }
+              {...register('password', { required: true })}
+              {...errors.password && <p>password is required.</p>}
+            />
           </label>
 
           <label>
@@ -182,9 +153,9 @@ export default function SignUp() {
               onChange={onDescriptionChange}
             />
           </label>
+
           <input type="submit" value="Sign Up" />
         </form>
-        
       </div>
       <Footer></Footer>
     </>
